7356032f584f3960525d4e77a736ade292421cc4
NPheonix <ns687575@gmail.com>
2026-04-20 02:02:52 +0530

    Data Ingestion

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
+from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_ingestion import DataIngestionConfig
 import sys
 
 if __name__=="__main__":
@@ -8,7 +10,10 @@ if __name__=="__main__":
 
 
     try:
-        a=1/0
+        #data_ingestion_config = DataIngetsionConfig()
+        data_ingestion = DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+
     except Exception as e:
         logging.info("Custome Exception")
         raise CustomException(e,sys)
